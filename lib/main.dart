accd456c2cd2d669b4ffc9ceef8675aa14cc58e5
Nagy Péter Géza <peternagy1332@sch.bme.hu>
2020-01-13 00:36:32 +0100

    flutter_braintree setup - throws exception

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_braintree/flutter_braintree.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+void main() => runApp(
+      MaterialApp(
+        home: MyApp(),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static final String tokenizationKey = 'sandbox_8hxpnkht_kzdtzv2btm4p7s5j';
+
+  void showNonce(BraintreePaymentMethodNonce nonce) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('Payment method nonce:'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text('Nonce: ${nonce.nonce}'),
+            SizedBox(height: 16),
+            Text('Type label: ${nonce.typeLabel}'),
+            SizedBox(height: 16),
+            Text('Description: ${nonce.description}'),
+          ],
+        ),
+      ),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Braintree example app'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            RaisedButton(
+              onPressed: () async {
+                var request = BraintreeDropInRequest(
+                  tokenizationKey: tokenizationKey,
+                  collectDeviceData: true,
+                  googlePaymentRequest: BraintreeGooglePaymentRequest(
+                    totalPrice: '4.20',
+                    currencyCode: 'USD',
+                    billingAddressRequired: false,
+                  ),
+                  paypalRequest: BraintreePayPalRequest(
+                    amount: '4.20',
+                    displayName: 'Example company',
+                  ),
+                );
+                BraintreeDropInResult result =
+                    await BraintreeDropIn.start(request);
+                if (result != null) {
+                  showNonce(result.paymentMethodNonce);
+                }
+              },
+              child: Text('LAUNCH NATIVE DROP-IN'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            RaisedButton(
+              onPressed: () async {
+                final request = BraintreeCreditCardRequest(
+                  cardNumber: '4111111111111111',
+                  expirationMonth: '12',
+                  expirationYear: '2021',
+                );
+                BraintreePaymentMethodNonce result =
+                    await Braintree.tokenizeCreditCard(
+                  tokenizationKey,
+                  request,
+                );
+                if (result != null) {
+                  showNonce(result);
+                }
+              },
+              child: Text('TOKENIZE CREDIT CARD'),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                final request = BraintreePayPalRequest(
+                  billingAgreementDescription:
+                      'I hearby agree that flutter_braintree is great.',
+                  displayName: 'Your Company',
+                );
+                BraintreePaymentMethodNonce result =
+                    await Braintree.requestPaypalNonce(
+                  tokenizationKey,
+                  request,
+                );
+                if (result != null) {
+                  showNonce(result);
+                }
+              },
+              child: Text('PAYPAL VAULT FLOW'),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                final request = BraintreePayPalRequest(amount: '13.37');
+                BraintreePaymentMethodNonce result =
+                    await Braintree.requestPaypalNonce(
+                  tokenizationKey,
+                  request,
+                );
+                if (result != null) {
+                  showNonce(result);
+                }
+              },
+              child: Text('PAYPAL CHECKOUT FLOW'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
